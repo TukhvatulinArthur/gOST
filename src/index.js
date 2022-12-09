d06af91d7ded2ab1c29f975692510deb5f82023e
TArtur <onese7enzzz@gmail.com>
2022-12-10 01:14:10 +0600

    changes

--- src/index.js
+++ src/index.js
@@ -312,13 +312,13 @@ const init = () => {
 
 init();
 
-window.addEventListener("load", async () => {
-  if ("serviceWorker" in navigator) {
-    try {
-      const reg = await navigator.serviceWorker.register("../sw.js");
-      console.log("Service worker register success", reg);
-    } catch (e) {
-      console.log("Service worker register fail");
-    }
-  }
-});
+// window.addEventListener("load", async () => {
+//   if ("serviceWorker" in navigator) {
+//     try {
+//       const reg = await navigator.serviceWorker.register("../sw.js");
+//       console.log("Service worker register success", reg);
+//     } catch (e) {
+//       console.log("Service worker register fail");
+//     }
+//   }
+// });
